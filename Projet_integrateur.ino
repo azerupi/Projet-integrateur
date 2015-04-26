3c4e158d3f60bedeaea04da863f333f8f114186e
Mathieu David <mathieudavid@mathieudavid.org>
2015-04-26 19:04:37 +0200

    We wired the buttons on the arduino, so I added them in the code and in the schematics

--- Projet_integrateur.ino
+++ Projet_integrateur.ino
@@ -39,6 +39,15 @@
 const char heating = 47;
 const char light = 45;
 
+// Buttons
+
+const char button_1 = 41; // Most left button
+const char button_2 = 43;
+const char button_3 = 45;
+const char button_4 = 47;
+const char button_5 = 49;
+const char button_6 = 51; // Most right button
+
 
 // LCD
 
